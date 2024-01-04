1a678b94e7749fe89ffba815d08bc683c65e2df4
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2024-01-04 20:08:14 -0300

    project act

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,20 +50,6 @@ const Navbar = () => {
          </div>
       </div>
 
-
-
-
-
-
-    // <div className='h-14 flex flex-row w-full items-center justify-between z-10 fixed md:static'>
-    //     <h3 className='mx-4 font-sansBold text-[#99D9D9] text-xl hover:animate-shake hover:animate-once hover:animate-duration-1000'><NavLink to='/'>Elizabeth M. Gurdzel</NavLink></h3>
-    //     <div className='mx-4 '>
-    //       <NavLink to="/about"  className='mx-2 font-sansRegular text-lg hover:border-b-2 hover:border-[#99D9D9]  text-[#99D9D9]'>About</NavLink>
-    //       <NavLink to="/projects"  className='mx-2 font-sansRegular text-lg hover:border-b-2 hover:border-[#99D9D9] text-[#99D9D9]' >Projects</NavLink>
-    //       <NavLink to="/tecnologies"  className='mx-2 font-sansRegular text-lg hover:border-b-2 hover:border-[#99D9D9] text-[#99D9D9]'>Tecnologies</NavLink>
-    //       <NavLink to="/contact" className='mx-2 font-sansRegular text-lg hover:border-b-2 hover:border-[#99D9D9] text-[#99D9D9]'>Contact</NavLink>
-    //     </div>
-    // </div>
   )
 }
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
 import caravane from "../assets/foto/carav.png"
 import vg from "../assets/foto/videogames_home.png"
+import ibmh from "../assets/foto/thumbnail.png"
+import ibmhVideo from "../assets/foto/scrnli_4_1_2024_19-14-34.mp4"
 import { NavLink } from 'react-router-dom'
 
 const Projects = () => {
 
     const linkCaravana = "https://caravana.onrender.com/";
     const repositoryVg = "https://github.com/elizabethMica/Videogames";
+    const linkIBMH = "https://ibmhurlinghamfront.onrender.com/"
 
   return (
     <div id='projects' className='flex flex-col justify-center items-center  bg-[#252525]  '>
         <h3 className='font-sansBold text-lg mt-20 text-white px-2 border-b-2 border-[#99D9D9] sm:text-2xl'>My Projects</h3>
         <div className='flex flex-col justify-center flex-wrap  items-center m-10 sm:flex-row'>
+
+        <div className='m-4 flex flex-col justify-center sm:w-3/4' >
+               <h4 className='font-sansBold text-white text-sm text-center my-2 sm:text-xl'>IBM Hurlingham</h4>
+               <p className='font-sansRegular text-gray-400 text-sm sm:text-lg text-center'> </p>
+               <video autoPlay>
+                <source src={ibmhVideo} type="video/mp4"/>
+                Your browser does not support the video tag.
+               </video>
+               <NavLink to={linkIBMH} target="_blank">
+                 <p className='font-sansRegular font-bold text-sm bg-[#99D9D9] text-black my-2 text-center sm:text-xl hover:bg-[#419999] rounded-full'>Click here to visit the website!</p>
+               </NavLink> 
+            </div>
+
+
         <div className='m-4 flex flex-col justify-center sm:w-3/4'>
                <h4 className='font-sansBold text-white text-sm text-center my-2 sm:text-xl'>Caravana Social</h4>
                <p className='font-sansRegular text-gray-400 text-sm text-center sm:text-lg'>The page consists of a job board that provides    users with the opportunity to receive training. Companies
@@ -38,6 +55,8 @@ are user-created or sourced from an external API.
                  <p className='font-sansRegular font-bold text-sm bg-[#99D9D9] text-black my-2 text-center sm:text-xl hover:bg-[#419999] rounded-full'>Wanna see the repository? Click here!</p>
                </NavLink> 
             </div>
+
+            
         </div>   
         <span className='text-xl font-bold text-[#99D9D9] font-sansBold p-3'>More projects to come!</span>
     </div>
